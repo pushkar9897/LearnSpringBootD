233fb1d7b44545c4f5961c571c5758dbbd3fb487
pushkar9897 <01pushkarguptapg@gmail.com>
2025-12-13 01:29:41 +0530

    i

---
+++ src/main/java/com/clf/JSON_DEMO/App.java
@@ -0,0 +1,25 @@
+package com.clf.JSON_DEMO;
+
+import com.clf.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) throws IOException {
+        Student student=new Student();
+        student.setCity("Deli");
+        student.setName("Niraj");
+        student.setCourse("JFS");
+
+        System.out.println(student);
+        System.out.println("-----------------------------------------");
+        ObjectMapper objectMapper=new ObjectMapper();
+        String object = objectMapper.writeValueAsString(student);
+        System.out.println(object);
+    }
+}
